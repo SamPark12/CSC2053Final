148e9ffa008de2e4466457d26c637e8621e35e74
SamPark12 <spark48@villanova.edu>
2022-12-05 16:03:12 -0500

    Added comments to help understand the heroes/detail code

--- src/app/hero.ts
+++ src/app/hero.ts
@@ -1,4 +1,6 @@
+//This code creates a Hero interface with id and name
 export interface Hero {
     id: number;
     name: string;
   }
+//This is exported and imported by heroes.component.ts
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,13 +1,14 @@
+//This code initializes the class HeroesComponenet
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
 
-@Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+@Component({ // decorator function that specifies the Angular metadata for the component.
+  selector: 'app-heroes', //The component's CSS element selector
+  templateUrl: './heroes.component.html',//The location of the component's template file.
+  styleUrls: ['./heroes.component.css']//The location of the component's private CSS styles.
 })
-
+//Always export the component class so you can import it elsewhere … like in the AppModule
 export class HeroesComponent implements OnInit {
 
   heroes = HEROES;
