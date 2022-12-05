9a768288672c40c6fef32175b237e867f2638a24
SamPark12 <spark48@villanova.edu>
2022-12-05 13:42:38 -0500

    Programed heroes folder to display heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
 
 @Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  selector: 'app-heroes', //The component's CSS element selector.
+  templateUrl: './heroes.component.html',//The location of the component's template file
+  styleUrls: ['./heroes.component.css']//The location of the component's private CSS styles.
 })
-export class HeroesComponent {
+export class HeroesComponent implements OnInit {
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 
 }
